c13bef115a19b3343b31b68799edb8ea09292b21
Omkar <omkareswarhota1234@gmail.com>
2024-06-21 12:19:34 +0530

    change

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 const mongoURL = process.env.MONGODB_URL
-const localUrl=process.env.LOCAL_URL
+//const localUrl=process.env.LOCAL_URL
 // Setup connection
 mongoose.connect(mongoURL, {
     useNewUrlParser: true,
